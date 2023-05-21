622520df6702f6137a49cb8f659d5effbed03c72
JIW00NG <ipkjw2@gmail.com>
2023-05-21 18:57:24 +0900

    [INIT] init project

---
+++ RoundTheTown/MVVM/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoundTheTown
+//
+//  Created by JiwKang on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
